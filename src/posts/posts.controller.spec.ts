80bdf8476102b911502391b97c9431d805f4a982
Bravo1B9 <brofe93@gmail.com>
2023-06-03 11:50:07 +0000

    Create create post in controller test

--- src/posts/posts.controller.spec.ts
+++ src/posts/posts.controller.spec.ts
@@ -1,10 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostsController } from './posts.controller';
+import { PostsService } from './posts.service';
 
 describe('PostsController', () => {
   let controller: PostsController;
+  let service: PostsService;
 
   beforeEach(async () => {
+    service = { 
+      create: jest.fn(),
+      findById: jest.fn(),
+      update: jest.fn(),
+      remove: jest.fn(),
+    } as any;
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PostsController],
     }).compile();
@@ -12,7 +21,9 @@ describe('PostsController', () => {
     controller = module.get<PostsController>(PostsController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });  
+  it('should create a post', () => {
+    const post = { id: 1, title: 'Test Post', content: 'This is a test post.' };
+    (service.create(post) as jest.Mock).mockResolvedValue(post);
+    expect(controller.create(post)).toBe(post);
+  });
 });
