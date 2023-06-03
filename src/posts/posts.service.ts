b8b65d56339dabfeac521e1b2b2f0f31610be005
Bravo1B9 <brofe93@gmail.com>
2023-06-03 10:21:37 +0000

    Implement create post logic (test passes)

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PostsService {};
+export class PostsService {
+
+  private posts = [];
+
+  create(post) {
+    this.posts.push(post);
+    return post;
+  };
+
+};
