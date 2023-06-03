786a5911118c56f591b4ffddef818540c7c89e5d
Bravo1B9 <brofe93@gmail.com>
2023-06-03 10:36:18 +0000

    Create find all posts test

--- src/posts/posts.service.spec.ts
+++ src/posts/posts.service.spec.ts
@@ -22,4 +22,13 @@ describe('PostsService', () => {
     service.create(post);
     expect(await service.findById(1)).toEqual(post);
   });
+
+  it('should get all posts', () => {
+    const post1 = { id: 1, title: 'Test Post 1', content: 'This is a test post.' };
+    const post2 = { id: 2, title: 'Test Post 2', content: 'This is another test post.' };
+    service.create(post1);
+    service.create(post2);
+    const posts = service.findAll();
+    expect(service.findAll()).toEqual([post1, post2]);
+  });
 });
