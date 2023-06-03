4fbb13edea39ef0b49e605e06a777f9656a555f9
Bravo1B9 <brofe93@gmail.com>
2023-06-03 12:02:23 +0000

    Add async and await

--- src/posts/posts.service.spec.ts
+++ src/posts/posts.service.spec.ts
@@ -12,9 +12,9 @@ describe('PostsService', () => {
     service = module.get<PostsService>(PostsService);
   });
 
-  it('should create a post', () => {
+  it('should create a post', async () => {
     const post = { id: 1, title: 'Test Post', content: 'This is a test post.' };
-    expect(service.create(post)).toEqual(post);
+    expect(await service.create(post)).toEqual(post);
   });
 
   it('should get a post for a given ID', async () => {
@@ -23,26 +23,26 @@ describe('PostsService', () => {
     expect(await service.findById(1)).toEqual(post);
   });
 
-  it('should get all posts', () => {
+  it('should get all posts', async () => {
     const post1 = { id: 1, title: 'Test Post 1', content: 'This is a test post.' };
     const post2 = { id: 2, title: 'Test Post 2', content: 'This is another test post.' };
     service.create(post1);
     service.create(post2);
     const posts = service.findAll();
-    expect(service.findAll()).toEqual([post1, post2]);
+    expect(await service.findAll()).toEqual([post1, post2]);
   });
 
-  it('should update a post', () => {
+  it('should update a post', async () => {
     const post = { id: 1, title: 'Test Post', content: 'This is a test post.' };
     service.create(post);
     const newPost = { id: 1, title: 'Updated Test Post', content: 'This is an updated test post.' };
     service.update(1, newPost);
-    expect(service.findById(1)).toEqual(newPost);
+    expect(await service.findById(1)).toEqual(newPost);
   });
 
-  it('should remove a post', () => {
+  it('should remove a post', async () => {
     const post = { id: 1, title: 'Test Post', content: 'This is a test post.' };
     service.create(post);
-    expect(service.remove(1)).toBeUndefined();
+    expect(await service.remove(1)).toBeUndefined();
   });
 });
