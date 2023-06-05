45face843b0cdac5e08689d4bba30822b22ee931
Bravo1B9 <brofe93@gmail.com>
2023-06-05 08:53:48 +0000

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
+require('dotenv').config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-  dotenv.config();
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
