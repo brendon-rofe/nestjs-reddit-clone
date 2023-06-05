46bc338723a3f3cb084cb110afb4620e57b1b422
Bravo1B9 <brofe93@gmail.com>
2023-06-05 08:20:21 +0000

    install and setup sqlite for testing

--- src/posts/posts.service.spec.ts
+++ src/posts/posts.service.spec.ts
@@ -1,16 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostsService } from './posts.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from './post.entity';
+import { Repository } from 'typeorm';
 
 describe('PostsService', () => {
   let service: PostsService;
+  let repository: Repository<Post>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [TypeOrmModule.forRoot({
+        type: 'sqlite',
+        database: ':memory:',
+        entities: [Post],
+        synchronize: true,
+      }), TypeOrmModule.forFeature([Post])],
       providers: [PostsService],
     }).compile();
 
     service = module.get<PostsService>(PostsService);
+    repository = module.get<Repository<Post>>('PostRepository');
   });
 
   
+
 });
