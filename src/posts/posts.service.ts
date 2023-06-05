842176257f3de8272581e813c37cb779696498e7
Bravo1B9 <brofe93@gmail.com>
2023-06-05 08:27:57 +0000

    Implement create post logic in service

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -2,11 +2,16 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Post } from './post.entity';
 import { Repository } from 'typeorm';
+import { CreatePostDto } from './dtos/create-post.dto';
 
 @Injectable()
 export class PostsService {
   constructor(@InjectRepository(Post) private postRepo: Repository<Post>) {};
 
-  
+  async create(post: CreatePostDto): Promise<Post> {
+    const newPost = await this.postRepo.create(post);
+    await this.postRepo.save(newPost);
+    return newPost;
+  };
 
 };
