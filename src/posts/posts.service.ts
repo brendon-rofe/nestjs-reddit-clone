29fb6c49152c909c64c291c967572dcb9be94964
Bravo1B9 <brofe93@gmail.com>
2023-06-10 11:18:59 +0000

    Import postRepo and entity inot posts.service.ts

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Post } from './post.entity';
 
 @Injectable()
-export class PostsService {};
+export class PostsService {
+  constructor(@InjectRepository(Post) private postRepo: Repository<Post>) {};
+};
