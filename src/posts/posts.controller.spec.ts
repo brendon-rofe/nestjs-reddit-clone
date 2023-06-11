2498c3a7e2486083080a9301d4f2bb99084da247
Bravo1B9 <brofe93@gmail.com>
2023-06-11 12:41:01 +0000

    Update and create create post test

--- src/posts/posts.controller.spec.ts
+++ src/posts/posts.controller.spec.ts
@@ -1,18 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostsController } from './posts.controller';
+import { PostsService } from './posts.service';
 
 describe('PostsController', () => {
   let controller: PostsController;
+  let service: PostsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PostsController],
+      providers: [PostsService]
     }).compile();
 
     controller = module.get<PostsController>(PostsController);
+    service = module.get<PostsService>(PostsService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should create a post', async () => {
+    const post = { title: 'Test Post', content: 'This is a test post.' };
+    expect(await controller.create(post)).toMatchObject(post);
   });
 });
