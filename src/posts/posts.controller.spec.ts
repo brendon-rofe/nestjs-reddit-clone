afc3baacbbec8cde004591b38bb01de332de5dbc
Bravo1B9 <brofe93@gmail.com>
2023-06-12 05:18:44 +0000

    Create find all posts test

--- src/posts/posts.controller.spec.ts
+++ src/posts/posts.controller.spec.ts
@@ -6,13 +6,15 @@ describe('PostsController', () => {
   let controller: PostsController;
   let service: PostsService;
 
-  const post = { title: 'Test Post', content: 'This is a test post.' };
+  const post1 = { title: 'First Test Post', content: 'This is the first test post.' };
+  const post2 = { title: 'First Test Post', content: 'This is the first test post.' };
 
   const mockPostsService = {
     create: jest.fn().mockImplementation((dto) => dto),
     findById: jest.fn().mockImplementation((id) => {
-      return { id, ...post };
+      return { id, ...post1 };
     }),
+    findAll: jest.fn().mockImplementation(() => [post1, post2]),
   };
 
   beforeEach(async () => {
@@ -31,13 +33,17 @@ describe('PostsController', () => {
   });
 
   it('should create a post', async () => {
-    const post = { title: 'Test Post', content: 'This is a test post.' };
-    expect(await controller.create(post)).toMatchObject(post);
+    expect(await controller.create(post1)).toMatchObject(post1);
   });
 
   it('should find a post by ID', async () => {
-    const post = { title: 'Test Post', content: 'This is a test post.' };
-    await controller.create(post);
-    expect(await controller.findById('1')).toMatchObject({ id: 1, ...post });
+    await controller.create(post1);
+    expect(await controller.findById('1')).toMatchObject({ id: 1, ...post1 });
+  });
+
+  it('should find all posts', async () => {
+    await controller.create(post1);
+    await controller.create(post2);
+    expect(await controller.findAll).toMatchObject([post1, post2]);
   });
 });
