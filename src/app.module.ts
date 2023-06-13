4c5b303fc755f18354fe6573c7d657604e043c26
Bravo1B9 <brofe93@gmail.com>
2023-06-13 13:58:05 +0000

    stuff

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
   imports: [
     DatabaseModule,
     PostsModule,
     DatabaseModule,
-    UsersModule
+    UsersModule,
+    AuthenticationModule
   ],
 })
 export class AppModule {}
