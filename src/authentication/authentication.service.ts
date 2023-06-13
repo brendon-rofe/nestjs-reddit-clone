af43bda8d055350459d6744ec2e253104cecb507
Bravo1B9 <brofe93@gmail.com>
2023-06-13 14:22:50 +0000

    Create registration functionality

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { UsersService } from 'src/users/users.service';
+import { RegisterDto } from './dtos/register.dto';
 
 @Injectable()
-export class AuthenticationService {};
+export class AuthenticationService {
+  constructor(private usersService: UsersService) {};
+
+  async register(registrationData: RegisterDto) {
+    const hashedPassword = await bcrypt.hash(registrationData.password, 10);
+    const newUser = await this.usersService.create({
+      ...registrationData,
+      passwordHash: hashedPassword
+    });
+    newUser.passwordHash = undefined;
+    return newUser;
+  };
+};
