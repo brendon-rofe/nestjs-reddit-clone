d34afc91d39b82e35af77dd94a3a76f24755ea8e
Bravo1B9 <brofe93@gmail.com>
2023-06-13 14:39:50 +0000

    Implement user registration

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthenticationService } from './authentication.service';
+import { RegisterDto } from './dtos/register.dto';
+import { User } from 'src/users/user.entity';
 
 @Controller('authentication')
-export class AuthenticationController {};
+export class AuthenticationController {
+  constructor(private authService: AuthenticationService) {};
+
+  @Post('/register')
+  async register(@Body() registrationData: RegisterDto): Promise<User> {
+    return await this.authService.register(registrationData);
+  };
+};
