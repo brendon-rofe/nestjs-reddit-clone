fb18c5c15bbf1205b59a75899fd94d5ca088bf29
Bravo1B9 <brofe93@gmail.com>
2023-06-13 13:26:18 +0000

    Initial users service and module setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
 import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     DatabaseModule,
     PostsModule,
-    DatabaseModule
+    DatabaseModule,
+    UsersModule
   ],
 })
 export class AppModule {}
