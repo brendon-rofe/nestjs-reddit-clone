7b401c0ac6aacd21ce8ef360abb40e5792b5cd33
Bravo1B9 <brofe93@gmail.com>
2023-06-14 08:55:16 +0000

    Implement login functionality

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { UsersService } from 'src/users/users.service';
-import { RegisterDto } from './dtos/register.dto';
+import { RegisterDto } from './dtos/register.dto'
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthenticationService {
-  constructor(private usersService: UsersService) {};
+  constructor(private usersService: UsersService, private jwtService: JwtService) {};
 
   async register(registrationData: RegisterDto) {
     const hashedPassword = await bcrypt.hash(registrationData.password, 10);
@@ -22,6 +23,13 @@ export class AuthenticationService {
     if(user && await bcrypt.compare(password, user.passwordHash)) {
       user.passwordHash = undefined;
       return user;
+    }
+  };
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload)
     }
   };
 };
