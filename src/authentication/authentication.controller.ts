06415b7db03e13dbd132e64c6f0307d52fba0db6
Bravo1B9 <brofe93@gmail.com>
2023-06-16 15:04:49 +0000

    Initial login

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -7,8 +7,13 @@ import { User } from 'src/users/user.entity';
 export class AuthenticationController {
   constructor(private authService: AuthenticationService) {};
 
-  @Post('/register')
+  @Post('register')
   async register(@Body() registrationData: RegisterDto): Promise<User> {
     return await this.authService.register(registrationData);
+  };
+
+  @Post('login')
+  async login() {
+    return { message: 'This route will log you in' };
   };
 };
