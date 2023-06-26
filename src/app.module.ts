6f48fba9ade7f19ab41af87b1ec1d8a52f3d7a38
Bravo1B9 <brofe93@gmail.com>
2023-06-26 05:07:26 +0000

    Initial auth module, controlle and service generation

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { PostsModule } from './posts/posts.module';
 import { UsersModule } from './users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { config } from 'dotenv';
+import { AuthModule } from './auth/auth.module';
 config();
 
 @Module({
@@ -14,7 +15,7 @@ config();
     }),
     DatabaseModule, 
     PostsModule, 
-    UsersModule
+    UsersModule, AuthModule
   ],
 })
 export class AppModule {};
