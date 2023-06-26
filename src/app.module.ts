88d8d9a9cd71ebd2132c59064114675618cdba5e
Bravo1B9 <brofe93@gmail.com>
2023-06-26 04:57:46 +0000

    Install deps and update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,19 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { PostsModule } from './posts/posts.module';
 import { UsersModule } from './users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { config } from 'dotenv';
+config();
 
 @Module({
-  imports: [DatabaseModule, PostsModule, UsersModule],
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET_KEY,
+      signOptions: { expiresIn: '1h' },
+    }),
+    DatabaseModule, 
+    PostsModule, 
+    UsersModule
+  ],
 })
-export class AppModule {}
+export class AppModule {};
