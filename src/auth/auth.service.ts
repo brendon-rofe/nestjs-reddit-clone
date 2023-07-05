c0fa71480283a548afb4e2b1456660be92a3242f
Bravo1B9 <brofe93@gmail.com>
2023-07-05 07:27:03 +0000

    Some more setup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+  constructor(private usersService: UsersService) {};
+
+  async register() {};
+
+  async login() {};
+
+};
