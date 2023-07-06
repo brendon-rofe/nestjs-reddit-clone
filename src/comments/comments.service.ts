69ce3c82ead43ee337913c57a5042df229bfcbdf
Bravo1B9 <brofe93@gmail.com>
2023-07-06 06:04:33 +0000

    Initial comments.service.ts setup

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CommentEntity } from './comment.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CommentsService {};
+export class CommentsService {
+
+  constructor(@InjectRepository(CommentEntity) commentRepo: Repository<CommentEntity>) {};
+
+  async create() {};
+
+  async getAllUserComments() {};
+
+  async getById() {};
+
+  async update() {};
+
+  async delete() {};
+
+};
