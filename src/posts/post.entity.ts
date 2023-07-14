928bf8b4b2228b25cd25dd546942c7880489e4a4
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2023-07-14 15:01:45 +0200

    Update

--- src/posts/post.entity.ts
+++ src/posts/post.entity.ts
@@ -1,6 +1,6 @@
 import { CommentEntity } from "src/comments/comment.entity";
 import { UserEntity } from "src/users/user.entity";
-import { Column, CreateDateColumn, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity('posts')
 export class PostEntity {
@@ -26,7 +26,8 @@ export class PostEntity {
   @OneToMany(() => CommentEntity, (comment) => comment.post)
   comments: CommentEntity[];
 
-  @ManyToOne(() => UserEntity, (author: UserEntity) => author.posts)
+  @ManyToOne(() => UserEntity)
+  @JoinColumn({ name: "author_id" })
   author: UserEntity;
 
   @Column()
